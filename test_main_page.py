0e45782d5d7e2fba6a33acc1475c8310061815e3
Max Kalinich <maximk@tradeix.com>
2019-10-18 16:36:24 +0300

    Added PageObject-related tests in test_main_page.py file and pages directory

---
+++ test_main_page.py
@@ -0,0 +1,22 @@
+# import pages.main_page
+from .pages.main_page import MainPage
+# from ..pages import main_page
+
+# from ..pages.login_page import LoginPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
