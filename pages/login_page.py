2ca671dbd795ecb8e3a5285f3b53125131a01e76
Max Kalinich <maximk@tradeix.com>
2019-10-22 18:34:51 +0300

    Final task for lesson 4.3

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,14 +8,21 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_EMAIL)
+        password_field = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PASSWORD_1)
+        password_confirm_field = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PASSWORD_2)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+        password_confirm_field.send_keys(password)
+        register_button.click()
+
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url, '"login" text is missed at the URL'
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is missed at the page"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is missed at the page"
